b52117dff80534f915147faf0f217d27d202acab
andreasjansson <andreas@replicate.ai>
2022-07-06 17:45:12 +0200

    load model in the prediction endpoint

--- app.py
+++ app.py
@@ -22,16 +22,21 @@ def predict():
         # TODO: this should happen in the model itself
         for path in starting_paths:
             if path["stroke_width"] == int(path["stroke_width"]):
-                path["stroke_width"] += .00001
+                path["stroke_width"] += 0.00001
             for i, color in enumerate(path["stroke_color"]):
                 if color == int(color):
-                    path["stroke_color"][i] += .00001
+                    path["stroke_color"][i] += 0.00001
             for i, point in enumerate(path["points"]):
                 for j, p in enumerate(point):
                     if p == int(p):
-                        path["points"][i][j] += .00001
+                        path["points"][i][j] += 0.00001
 
         starting_paths = json.dumps(starting_paths)
+
+    model = replicate.models.get("evilstreak/clipdraw-interactive")
+    version = model.versions.get(
+        "8feca8c65270d6ea1b30080a5dc31382afc8316c895299add625ff97a789554c"
+    )
 
     resp = version.predict(
         prompt=prompt,
@@ -49,8 +54,4 @@ def send_static(path):
 
 
 if __name__ == "__main__":
-    model = replicate.models.get("evilstreak/clipdraw-interactive")
-    version = model.versions.get(
-        "8feca8c65270d6ea1b30080a5dc31382afc8316c895299add625ff97a789554c"
-    )
     app.run(debug=True)
